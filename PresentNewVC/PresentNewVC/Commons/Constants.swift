38d5ae7f93f1c67dcf23c6dc1b322a768c3b5be8
Marlon David Ruiz Arroyave <Marlon.X.Ruiz.-ND@disney.com>
2019-03-06 15:19:45 -0500

    Add Constants file and create testUser there.

---
+++ PresentNewVC/PresentNewVC/Commons/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  PresentNewVC
+//
+//  Created by Marlon David Ruiz Arroyave on 3/6/19.
+//  Copyright © 2019 Eafit. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    struct TestUser {
+        static let Username = "mruiz"
+        static let Password = "swift"
+    }
+}
