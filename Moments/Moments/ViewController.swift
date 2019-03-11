47c3b373d83eb18e34223346689e6c64303b4cfa
Marlon David Ruiz Arroyave <Marlon.X.Ruiz.-ND@disney.com>
2019-03-11 14:36:08 -0500

    Add moments project.

---
+++ Moments/Moments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moments
+//
+//  Created by Marlon David Ruiz Arroyave on 3/11/19.
+//  Copyright © 2019 Eafit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
